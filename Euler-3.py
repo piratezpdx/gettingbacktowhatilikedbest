4c9d3a56e53395be1d7820671058cabeb9591005
George Nicol <piratezpdx@users.noreply.github.com>
2020-01-06 14:45:25 -0800

    Create Euler-3.py

---
+++ Euler-3.py
@@ -0,0 +1,17 @@
+# What is the largest prime factor of the number 600851475143
+
+def prime_factor_finder(num):
+    print(num)
+    factors_found = set()
+    found = False
+    for denom in range(2,num):
+        if num % denom == 0:
+            found = True
+            factors_found = (prime_factor_finder(int(num/denom)))
+            factors_found.add(denom)
+            break
+
+    if not found:
+        factors_found.add(num)
+
+    return factors_found
