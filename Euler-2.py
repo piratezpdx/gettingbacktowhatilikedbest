95f1160b18071cd02d79cd4c3b306c30b715936f
George Nicol <piratezpdx@users.noreply.github.com>
2020-04-12 08:04:18 -0400

    update for pylint3.8 compliance

--- Euler-2.py
+++ Euler-2.py
@@ -1,20 +1,25 @@
-# python3
-# By considering the terms in the Fibonacci sequence, find the sum of the even-valued terms.
-# assumes user input is reasonable - like not an emoji or whatever
+'''
+python3.8
+By considering the terms in the Fibonacci sequence, find the sum of the even-valued terms.
+assumes user input is reasonable - like not an emoji or whatever
+'''
 
 def euler2(limit):
-    a = 1 #euler sequence terms
-    b = 2 #euler sequence terms
-    c = 3 #euler sequence terms, kludge to 3 but can leave as 0. just depends on your thinking.
+    '''
+    limit is what number to stop the sequence at or below
+    '''
+    
+    first = 1 #euler sequence terms
+    second = 2 #euler sequence terms
+    next_term = 3 #euler sequence terms, kludge to 3 but can leave as 0
     total = 2 # account for b term at start
-    euler_list = [1,2]
-    while c <= limit:
-        if c % 2 == 0:
-            total += c
-        c = a + b
-        # euler_list.append(c) # results in an extra term on list at end that is higher than limit. Can avoid with kludge or recursive solution
-        a = b
-        b = c
+    # euler_list = [1, 2]
+    while next_term <= limit:
+        if next_term % 2 == 0:
+            total += next_term
+        next_term = first + second
+        first = second
+        second = next_term
     # print(euler_list)
     # print(sum(filter(lambda x: x%2 == 0,euler_list[:-1])))
     print(total)
