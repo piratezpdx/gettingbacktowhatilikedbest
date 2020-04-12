6edca8e8eaf4aebe93e261508d28fda09a6bf102
George Nicol <piratezpdx@users.noreply.github.com>
2020-04-12 09:16:17 -0400

    update for pylint 3.8 compliance

--- Euler-6.py
+++ Euler-6.py
@@ -1,14 +1,18 @@
-# https://projecteuler.net/problem=6
-# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+'''
+Euler 6
+Find the difference between the sum of the squares of the
+first one hundred natural numbers and the square of the sum.
+invoke as:
+euler6(100)
+'''
 
 def euler6(num):
-    # sum_of_numbers = n(n+1)/2
+    '''
+    algorithm: sum_of_numbers = n(n+1)/2
+    '''
+
     sum_of_numbers = int(num*(num+1)/2)
     square_of_numbers_summed = 0
-    for number in range(1,num+1):
-        square_of_numbers_summed += number*number
+    for number in range(1, num+1):
+        square_of_numbers_summed += number**2
     print(sum_of_numbers**2 - square_of_numbers_summed)
-
-
-# invoke as:
-# euler6(100)
