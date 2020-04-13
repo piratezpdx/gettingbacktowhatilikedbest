e438daf19a666767e72597a95fae0377bff78f87
George Nicol <piratezpdx@users.noreply.github.com>
2020-04-13 10:01:42 -0400

    update for unittest usability

--- Euler-2.py
+++ Euler-2.py
@@ -1,13 +1,13 @@
-'''
-python3.8
+"""
+Euler 2 - python3.8
 By considering the terms in the Fibonacci sequence, find the sum of the even-valued terms.
 assumes user input is reasonable - like not an emoji or whatever
-'''
+"""
 
 def euler2(limit):
-    '''
+    """
     limit is what number to stop the sequence at or below
-    '''
+    """
     
     first = 1 #euler sequence terms
     second = 2 #euler sequence terms
@@ -22,4 +22,5 @@ def euler2(limit):
         second = next_term
     # print(euler_list)
     # print(sum(filter(lambda x: x%2 == 0,euler_list[:-1])))
-    print(total)
+    # print(total)
+    return total
